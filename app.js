bccc4860d2e2ec83024e4b88ac9fa2df348c6de0
Annelies-P <annelies.pauwels@gmail.com>
2020-11-10 17:32:05 +0100

    Deprecate old and release new DCAT dataset

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
 import { app, errorHandler } from 'mu';
-import { getRunningReleaseTask, getNextReleaseTask } from './lib/release-task';
+import { getRunningReleaseTask, getNextReleaseTask, TASK_NOT_STARTED_STATUS } from './lib/release-task';
+import flatten from 'lodash.flatten';
+import bodyParser from 'body-parser';
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.post('/delta', async function (req, res, next) {
   const isRunning = await getRunningReleaseTask();
 
+
   if (!isRunning) {
-    const task = await getNextReleaseTask();
-    if (task) {
-      console.log(`Start releasing new DCAT data`);
-      try {
-        task.execute();
+    const delta = req.body;
+    const inserts = flatten(delta.map(changeSet => changeSet.inserts));
+    const statusTriples = inserts.filter((t) => {
+      return t.predicate.value == 'http://www.w3.org/ns/adms#status'
+        && t.object.value == TASK_NOT_STARTED_STATUS;
+    });
+
+    if (statusTriples.length) {
+      console.log(`Found ${statusTriples.length} release tasks.`);
+      const task = await getNextReleaseTask();
+      if (task) {
+        console.log(`Start releasing new DCAT data`);
+        task.execute(); // errors are handled inside task.execute()
         return res.status(202).end();
-      } catch (e) {
-        console.log(`Something went wrong while releasing the data. Closing release task with failure state.`);
-        console.trace(e);
-        await task.closeWithFailure()();
-        return next(new Error(e));
+      } else {
+        console.log(`No scheduled release task found.`);
+        return res.status(200).end();
       }
     } else {
-      console.log(`No scheduled release task found. Did the insertion of a new task just fail?`);
-      return res.status(200).end();
+      console.log(`No triples found in the delta message.`);
+      res.status(200).end();
     }
+  } else {
+    console.log('No release task found in delta message.');
+    res.end('No release task found in delta message.');
   }
 });
+
 
 
 app.use(errorHandler);
--- config.js
+++ config.js
@@ -1,9 +1,23 @@
-const BESLUITVORMING_CATALOG_URI = process.env.BESLUITVORMING_CATALOG_URI || 'http://themis.vlaanderen.be/id/catalog/1e4733c1-7701-4f99-b3db-f5d348a7bc4b';
-const VERGADERACTIVITEIT_TYPE = process.env.VERGADERACTIVITEIT_TYPE || 'http://data.vlaanderen.be/ns/besluit#Zitting';
+const BESLUITVORMING_CATALOG_URI = 'http://themis.vlaanderen.be/id/catalog/1e4733c1-7701-4f99-b3db-f5d348a7bc4b';
+const VERGADERACTIVITEIT_TYPE = 'http://data.vlaanderen.be/ns/besluit#Vergaderactiviteit';
+const DATASET_TYPE = 'http://themis.vlaanderen.be/id/concept/dataset-type/9119805f-9ee6-4ef1-9ef7-ad8dccc2bf2d';
+const DATASET_NEWSITEM_TYPE = 'http://themis.vlaanderen.be/id/concept/distribution-type/dd5bfc23-8f88-4df5-80f6-a9f72e08d7c4';
+const DATASET_ATTACHMENT_TYPE = 'http://themis.vlaanderen.be/id/concept/distribution-type/c4d99dde-3df9-4da1-8136-9a3b2de82de4';
 const MU_APPLICATION_GRAPH = 'http://mu.semte.ch/graphs/publication-tasks';
+const PUBLIC_GRAPH = 'http://mu.semte.ch/graphs/public';
+const APPLICATION_DOMAIN = process.env.APPLICATION_DOMAIN || 'https://themis.vlaanderen.be';
+const UPDATE_BATCH_SIZE = parseInt(process.env.UPDATE_BATCH_SIZE) || 100;
+const SELECT_BATCH_SIZE = parseInt(process.env.SELECT_BATCH_SIZE) || 1000;
 
 export {
   BESLUITVORMING_CATALOG_URI,
   VERGADERACTIVITEIT_TYPE,
-  MU_APPLICATION_GRAPH
+  DATASET_TYPE,
+  DATASET_NEWSITEM_TYPE,
+  DATASET_ATTACHMENT_TYPE,
+  MU_APPLICATION_GRAPH,
+  PUBLIC_GRAPH,
+  APPLICATION_DOMAIN,
+  UPDATE_BATCH_SIZE,
+  SELECT_BATCH_SIZE
 };
